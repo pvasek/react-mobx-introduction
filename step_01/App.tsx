a8bf10c15581b2d741b30804dc56219c75b5f4a3
pospisil.v <pospisil.v@st-software.com>
2017-05-22 10:52:53 +0200

    step 1 and 2

--- step_01/App.tsx
+++ step_01/App.tsx
@@ -1,35 +1,35 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { restService } from '../common';
+import { PersonDetail } from './PersonDetail';
 
 export class App extends Component<{}, {}> {
+    constructor(){
+        super();
+        this.load = this.load.bind(this);
+    }
+    state = {
+        person: null
+    }
+
+    load(e: React.MouseEvent<HTMLButtonElement>){
+        //alert("Clicked");
+        restService.get('Person', '1').then(person => {
+            //alert(person.FirstName);
+            this.setState({person: person});
+        })
+    }
 
     render() {
-        const person = {
-            FirstName: 'Static',
-            LastName: 'Person'
-        };
+       
 
         return (
             <div>
                 <div>
-                    <button>Load</button>
+                    <button onClick={this.load}>Load</button>
                     <button>Save</button>
                 </div>
-                <div className="form">
-                    <div>
-                        <label>First Name:</label>
-                        <input type="text" value={person.LastName}/>
-                    </div>
-                    <div>
-                        <label>Last Name:</label>
-                        <input type="text" value={person.FirstName}/>
-                    </div>
-                    <div>
-                        <label>Country:</label>
-                        <select />
-                    </div>
-                </div>
+                <PersonDetail person={this.state.person} />
             </div>
         );
     }
--- step_01/PersonDetail.tsx
+++ step_01/PersonDetail.tsx
@@ -4,3 +4,29 @@ import { Component } from 'react';
 export interface IPersonDetailProps {
     person: any;
 }
+
+export class PersonDetail extends Component<IPersonDetailProps, {}>{
+    render(){
+        if(this.props.person === null){
+              return <span>Not Loaded</span>  
+            }
+        else{
+            return (
+                <div className="form">
+                        <div>
+                            <label>First Name:</label>
+                            <input type="text" value={this.props.person.FirstName}/>
+                        </div>
+                        <div>
+                            <label>Last Name:</label>
+                            <input type="text" value={this.props.person.LastName}/>
+                        </div>
+                        <div>
+                            <label>Country:</label>
+                            <select />
+                        </div>
+                    </div>
+            )
+        }
+    }
+}
